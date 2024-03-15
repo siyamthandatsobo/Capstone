57aa40a44039a24482969f1f3e83d4b4d1805545
Siyamthanda Tsobo <siyamthandatsobo@gmail.com>
2024-03-15 16:32:57 +0200

    added 401 error handling

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,13 +4,13 @@ const authenticate = (req, res, next) => {
     const tokenCookie = req.cookies.jwt;
 
     if (!tokenCookie) {
-        return res.status(403).send({ error: 'Missing JWT token in cookies' });
+        return res.status(401).send({ error: 'Missing JWT token in cookies' });
     }
 
     jwt.verify(tokenCookie, process.env.SECRET_KEY, (err, user) => {
         if (err) {
             console.error('JWT Verification Error:', err.message);
-            return res.status(403).send({ error: 'Failed to authenticate JWT token' });
+            return res.status(401).send({ error: 'Failed to authenticate JWT token' });
         }
 
         console.log('Decoded User:', user);
