dda54b298e0c671058c52f98a0585632591bbb41
Siyamthanda Tsobo <siyamthandatsobo@gmail.com>
2024-03-21 01:25:41 +0200

    updated

--- index.js
+++ index.js
@@ -62,7 +62,10 @@ const auth = async (req, res, next) => {
 
             const token = jwt.sign(tokenPayload, process.env.SECRET_KEY, { expiresIn: '2h' });
             console.log(token);
-            res.cookie('jwt', token);
+            res.cookie('jwt', token, {
+                sameSite: 'None', // You can set it to 'strict' or 'lax' depending on your requirements
+                // Other cookie options...
+              });
             res.json({
                 msg: 'You have logged in',
                 user: userInfo,
